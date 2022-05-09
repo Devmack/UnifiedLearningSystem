7c2bc692e8a92aa0010417d974fcbe82d8f6e576
Dominik <starzykdominik93@gmail.com>
2022-05-09 17:25:10 +0200

    Add domain related repository interface

---
+++ UnifiedLearningSystem.Domain/Repository/ILessonRepository.cs
@@ -0,0 +1,14 @@
+﻿using UnifiedLearningSystem.Domain.Entities;
+
+namespace UnifiedLearningSystem.Domain.Repository
+{
+    public interface ILessonRepository
+    {
+        public Task<ICollection<LearningTask>> GetAll();
+        public Task<ICollection<LearningTask>> GetSubsetBasedOn(Func<LearningTask, bool> subsetPredicate);
+        public Task<LearningTask> GetSingle(Guid id);
+        public Task<bool> Add(LearningTask task);
+        public Task<bool> Delete(LearningTask task);
+
+    }
+}
